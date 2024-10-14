ace4051c8f5b5871b881ce396e502a8b3db8adbf
epan <ericpanbot@gmail.com>
2024-10-14 03:12:09 -0700

    NNData pass 6 tests

---
+++ NNData.py
@@ -0,0 +1,85 @@
+"""Implementing the NNData Class."""
+from enum import Enum
+from collections import deque
+import numpy as np
+import random
+
+
+class Order(Enum):
+    """Indicate if training data shown in different or same order."""
+    SHUFFLE = 1
+    STATIC = 2
+
+
+class Set(Enum):
+    """Indicate whether requesting training or testing data."""
+    TRAIN = 1
+    TEST = 2
+
+
+class NNData:
+    """NNData manages training and testing data."""
+
+    @staticmethod
+    def percentage_limiter(percentage: float):
+        """Return percentage clamped between 0 and 1."""
+        if percentage < 0:
+            return 0
+        if percentage > 1:
+            return 1
+        return percentage
+
+    def load_data(self, features, labels):
+        """Add features and labels to the class."""
+        match (features, labels):
+            case (None, _) | (_, None):
+                self._features = None
+                self._labels = None
+                self.split_set()
+                return
+            case (f, l) if len(f) is not len(l):
+                self._features = None
+                self._labels = None
+                self.split_set()
+                raise ValueError('Mismatched features and labels length')
+            case _:
+                try:
+                    self._features = np.array(features, dtype=float)
+                    self._labels = np.array(labels, dtype=float)
+                except (ValueError, TypeError):
+                    # ValueError eg, ['a', 'b'] - Not Float
+                    # TypeError eg, 6 - Not Iterable
+                    self._features = None
+                    self._labels = None
+                    self.split_set()
+                    raise ValueError('Numpy array construction failed.')
+        self.split_set()
+
+    def __init__(self, features=None, labels=None, train_factor=0.9,):
+        """Initialize NNData with data."""
+        self._features = None
+        self._labels = None
+        self._train_indices = []
+        self._test_indices = []
+        self._train_pool = deque()
+        self._test_pool = deque()
+        self._train_factor = NNData.percentage_limiter(train_factor)
+        self.load_data(features, labels)
+
+    def split_set(self, new_train_factor=None):
+        if new_train_factor is not None:
+            self._train_factor = NNData.percentage_limiter(new_train_factor)
+        if self._features is None:
+            self._train_indices = []
+            self._test_indices = []
+            return
+        num_examples = self._features.size
+        num_examples_for_testing = (
+                num_examples - int(num_examples * self._train_factor))
+        indices = list(range(num_examples))
+        test_indices = random.sample(indices, num_examples_for_testing)
+        train_indices = [i for i in indices if i not in test_indices]
+        random.shuffle(train_indices)
+        self._train_indices = train_indices
+        self._test_indices = test_indices
+
