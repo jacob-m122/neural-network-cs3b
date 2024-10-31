0c2bc362146b67484583cca55d7c0ab643eb924f
jacob-m122 <123427189+jacob-m122@users.noreply.github.com>
2024-10-31 15:52:58 -0700

    Initial-commit one-pycode and docstyle resolved.

---
+++ FFNeurode.py
@@ -0,0 +1,40 @@
+"""Implement FFNeurode Class."""
+from __future__ import annotations
+from Neurode import Neurode
+from Neurode import MultiLinkNode
+from math import exp
+
+
+class FFNeurode(Neurode):
+    """Inherit from parent, Neurode, implement feed-forward process."""
+
+    @staticmethod
+    def _sigmoid(value: float):
+        """Return sigmoid function result."""
+        return 1 / (1 + exp(-value))
+
+    def _calculate_value(self):
+        """Calculate sum of weighted upstream node values."""
+        weighted_sum = 0
+        for node in self._neighbors[MultiLinkNode.Side.UPSTREAM]:
+            node_value = node.value
+            weight = self.get_weight(node)
+            weighted_sum += node_value * weight
+        self._value = self._sigmoid(weighted_sum)
+
+    def _fire_downstream(self):
+        """Indicate to downstream neighbors that node has data ready."""
+        for neighbor in self._neighbors[MultiLinkNode.Side.DOWNSTREAM]:
+            neighbor.data_ready_upstream(self)
+
+    def data_ready_upstream(self, node: Neurode):
+        """Handle data from upstream node."""
+        if self._check_in(node, MultiLinkNode.Side.UPSTREAM):
+            self._calculate_value()
+            self._fire_downstream()
+
+    def set_input(self, input_value: float):
+        """Set input node value, indicate to downstream nodes."""
+        self._value = input_value
+        for neighor in self._neighbors[MultiLinkNode.Side.DOWNSTREAM]:
+            neighbor.data_ready_upstream(self)
