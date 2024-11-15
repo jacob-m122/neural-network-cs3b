9c22d146dde0ef9855046054399fc9fdcc9ee8fb
jacob-m122 <123427189+jacob-m122@users.noreply.github.com>
2024-11-14 20:23:53 -0800

    Commit two-pycode and doc style fixed. layer name variables fixed to prevent confusion.

--- LayerList.py
+++ LayerList.py
@@ -1,10 +1,20 @@
 """Implement the LayerList class for input, hidden, and output layer management."""
+
+
 from DoublyLinkedList import DoublyLinkedList
 from Neurode import Neurode
 
+
 class LayerList(DoublyLinkedList):
-    """implement LayerList class"""
+    """implement LayerList class."""
+
     def __init__(self, inputs: int, outputs: int, neurode_type: type(Neurode)):
+        """
+        Initialize superclass, private neurode_type, input layer, output layer.
+
+        Add input layer to head and output layer after current.
+        Link input and output layers.
+        """
         super().__init__()
         self._neurode_type = neurode_type
         self.input_layer = [
@@ -17,16 +27,17 @@ class LayerList(DoublyLinkedList):
         self.add_after_current(self.output_layer)
         self.linking_helper(self.input_layer, self.output_layer)
 
-    def linking_helper(self, layer_upstream, layer_downstream):
+    def linking_helper(self, upstream_layer, downstream_layer):
         """Link neighboring layers."""
-        for neurode in layer_upstream:
-            neurode.reset_neighbors(layer_downstream, Neurode.Side.DOWNSTREAM)
-        for neurode in layer_downstream:
-            neurode.reset_neighbors(layer_upstream, Neurode.Side.UPSTREAM)
+        for neurode in upstream_layer:
+            neurode.reset_neighbors(downstream_layer, Neurode.Side.DOWNSTREAM)
+        for neurode in downstream_layer:
+            neurode.reset_neighbors(upstream_layer, Neurode.Side.UPSTREAM)
 
     def add_layer(self, num_nodes: int):
         """
         Ensure current layer is not output layer (tail).
+
         Insert hidden layer between input and output layers, link layers.
         """
         if self._curr == self._tail:
@@ -35,21 +46,25 @@ class LayerList(DoublyLinkedList):
             self._neurode_type() for _ in range(num_nodes)
         ]
         self.add_after_current(new_hidden_layer)
-        layer_upstream = self._curr.data
+        upstream_layer = self._curr.data
         self.move_forward()
-        self.linking_helper(layer_upstream, new_hidden_layer)
-        layer_downstream = self._curr.next.data
-        self.linking_helper(new_hidden_layer, layer_downstream)
+        self.linking_helper(upstream_layer, new_hidden_layer)
+        downstream_layer = self._curr.next.data
+        self.linking_helper(new_hidden_layer, downstream_layer)
 
     def remove_layer(self):
-        """Remove specific layer, ensure it is not removing output layer (tail)"""
+        """
+        Remove specific layer.
+
+        Ensure it is not removing output layer (tail).
+        """
         if self._curr.next == self._tail:
             raise IndexError
-        layer_upstream = self._curr.data
+        upstream_layer = self._curr.data
         self.remove_after_current()
         if self._curr.next:
-            layer_downstream = self._curr.next.data
-            self.linking_helper(layer_upstream, layer_downstream)
+            downstream_layer = self._curr.next.data
+            self.linking_helper(upstream_layer, downstream_layer)
 
     @property
     def input_nodes(self):
@@ -60,6 +75,7 @@ class LayerList(DoublyLinkedList):
     def output_nodes(self):
         """Access output layer neurodes."""
         return self._tail.data
+
 
 """
 Layerlist created.
